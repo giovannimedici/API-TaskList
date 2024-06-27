f9e758c53710d4aefb11777e036f749ee51c8a4a
giovannimedici <giovannigilsu@gmail.com>
2024-06-26 21:25:18 -0300

    second

--- server.js
+++ server.js
@@ -1,31 +1,52 @@
 import { fastify } from 'fastify'
+import { DatabasePostgres } from './dbPostgres.js'
+import cors from '@fastify/cors'
 
 const server = fastify()
+await server.register(cors, { 
+  // put your options here
+})
 
-const tasks = [
-    {
-        id: 1,
-        description: 'Oi'
-    },
-    {
-        id: 2,
-        description: 'Boa noite'
-    }
-]
+const database = new DatabasePostgres()
 
-server.get('/tasks', async => {
-    return tasks;
+
+
+server.get('/tasks', async (request) => {
+    const search = request.query.search
+
+    const tasks = await database.list(search)
+
+    return tasks
+
 })
 
 server.post('/tasks', async (request, reply) => {
-    const {id, description} = request.body
+    const {title} = request.body
 
-    await tasks.push({
-        id,
-        description
+    await database.create({
+        title
     })
 
     return reply.status(201).send()
+})
+
+server.put('/tasks/:id', async (request, reply) => {
+    const taskId = request.params.id
+    const {title} = request.body
+
+    await database.update(taskId, {
+        title
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/tasks/:id', async (request, reply) => {
+    const taskId = request.params.id
+
+    await database.delete(taskId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
